c321d510b0fbc2f5c1bfdfa750fb4b5a01b807cd
rushigithub12 <rushikeshkamble9256@gmail.com>
2024-05-14 00:01:35 +0530

    customers screen

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,10 +1,39 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import {
+  CustomersTableSkeleton,
+  InvoicesTableSkeleton,
+} from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
-import React from 'react';
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function page() {
-  return <div>Customers page</div>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
