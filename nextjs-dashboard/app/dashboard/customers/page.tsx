475ca0a8e87948210157213fe50c2cc317f585aa
rushigithub12 <rushikeshkamble9256@gmail.com>
2024-05-13 22:56:39 +0530

    added metadata

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
 export default function page() {
-  return (
-    <div>Customers page</div>
-  )
+  return <div>Customers page</div>;
 }
