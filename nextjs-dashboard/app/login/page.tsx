45d1a4066271a79dd56b8a7c96f4fd6d69880e9d
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-01-11 22:27:05 +0530

    use suspense inside server componentv when using it from urlsearchparam

--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
- 
+import { Suspense } from 'react';
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
@@ -10,7 +11,9 @@ export default function LoginPage() {
             <AcmeLogo />
           </div>
         </div>
-        <LoginForm />
+        <Suspense fallback={null}>
+          <LoginForm />
+        </Suspense>
       </div>
     </main>
   );
