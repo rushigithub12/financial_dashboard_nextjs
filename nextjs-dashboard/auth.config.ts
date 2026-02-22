0d7a4043e458bede90e102943b8af96eeb71e56d
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-02-22 18:11:39 +0530

    correct authentication routes

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,5 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -9,29 +9,30 @@ export const authConfig = {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       const isOnLogin = nextUrl.pathname === '/login';
-      
-      // Allow authenticated users to access dashboard
+      const isOnRoot = nextUrl.pathname === '/';
+
       if (isOnDashboard) {
         return isLoggedIn;
       }
-      
-      // Redirect authenticated users away from login page to dashboard
+
       if (isOnLogin && isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
-      
-      // Allow unauthenticated users to access login page
-      if (isOnLogin) {
+
+      if (isOnRoot && isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+
+      if (isOnLogin || isOnRoot) {
         return true;
       }
-      
-      // Redirect unauthenticated users to login
+
       if (!isLoggedIn) {
         return false;
       }
-      
+
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [],
 } satisfies NextAuthConfig;
